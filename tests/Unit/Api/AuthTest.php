d683ee7779da6ad121f55f5ffdab3c059a24eb87
Hengky Irianto <hengky.dev@gmail.com>
2018-09-15 01:25:20 +0700

    testing

--- tests/Unit/Api/AuthTest.php
+++ tests/Unit/Api/AuthTest.php
@@ -9,6 +9,25 @@ use App\User;
 
 class AuthTest extends TestCase
 {
+    public function testRegister()
+    {
+        User::where('email','admin@gmail.com')->delete();
+
+        $response = $this->json('POST', '/api/register', [
+            'name'          => 'Admin',
+            'email'         => 'admin@gmail.com',
+            'password'      => 'admin123',
+            'c_password'    => 'admin123'
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'success'   => [
+                    'token' => true
+                ]
+            ]);
+    }
     /**
      * A test for signin a api with laravel passport
      * credentials
@@ -30,23 +49,5 @@ class AuthTest extends TestCase
             ]);
     }
 
-    public function testRegister()
-    {
-        User::where('email','admin@gmail.com')->delete();
-
-        $response = $this->json('POST', '/api/register', [
-            'name'          => 'Admin',
-            'email'         => 'admin@gmail.com',
-            'password'      => 'admin123',
-            'c_password'    => 'admin123'
-        ]);
-
-        $response
-            ->assertStatus(200)
-            ->assertJson([
-                'success'   => [
-                    'token' => true
-                ]
-            ]);
-    }
+   
 }
